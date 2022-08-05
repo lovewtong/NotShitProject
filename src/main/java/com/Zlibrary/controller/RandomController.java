f33ae9d7cb6ea0f49e27b01505cd2699dd254406
wusy568 <wtwsy99@gmail.com>
2022-08-05 17:13:05 +0800

    第一次提交

---
+++ src/main/java/com/Zlibrary/controller/RandomController.java
@@ -0,0 +1,13 @@
+package com.Zlibrary.controller;
+
+/**
+ * @className: RandomController
+ * @description: TODO 类描述
+ * @author: sy
+ * @date: 2022-08-05
+ **/
+public class RandomController {
+
+
+
+}
