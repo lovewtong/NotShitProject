89da2d4143c546474bcd21da03878fc1d6096cc4
lovewutong <suqin2106@gmail.com>
2023-07-18 17:23:43 +0800

    123

--- src/main/resources/notShitProject/src/views/homePage/homePage.vue
+++ src/main/resources/notShitProject/src/views/homePage/homePage.vue
@@ -1,195 +1,204 @@
 <template>
-    <div class="home-page-container">
-        <div class="main-background"></div>
-        <div class="main-head">
-            <div class="main-title">
-                <div class="main-title-name">飞屎计划</div>
-                <div class="main-title-description">Not Shit Project</div>
-            </div>
-            <div class="main-tabs">
-                <div class="main-tabs-item" :class="currentType === 'dayRead' ? 'selected-main-tab' : ''"
-                    @click="changeTab('dayRead')">
-                    <span>每日阅读</span>
-                </div>
-                <div class="main-tabs-item" :class="currentType === 'other' ? 'selected-main-tab' : ''"
-                    @click="changeTab('other')">
-                    <span>其他东西</span>
-                </div>
-            </div>
+  <div class="home-page-container">
+    <div class="main-background"></div>
+    <div class="main-head">
+      <div class="main-title">
+        <div class="main-title-name">飞屎计划</div>
+        <div class="main-title-description">Not Shit Project</div>
+      </div>
+      <div class="main-tabs">
+        <div class="main-tabs-item" :class="currentType === 'dayRead' ? 'selected-main-tab' : ''"
+             @click="changeTab('dayRead')">
+          <span>每日阅读</span>
         </div>
-        <div class="main-container">
-            <div v-if="currentType === 'dayRead'" class="day-read-container">
-                <day-read></day-read>
-            </div>
-            <div class="other-info-container">
-                <div class="other-info">飞屎计划 @ 2022</div>
-                <div class="other-info">备案号</div>
-                <a target="_blank" rel="nofollow" href="https://beian.miit.gov.cn" class="other-info beian">
-                    苏ICP备2022041752号-1</a>
-            </div>
+        <div class="main-tabs-item" :class="currentType === 'other' ? 'selected-main-tab' : ''"
+             @click="changeTab('other')">
+          <span>其他东西</span>
         </div>
+      </div>
     </div>
+    <div class="main-container">
+      <div v-if="currentType === 'dayRead'" class="day-read-container">
+        <day-read></day-read>
+      </div>
+      <div class="other-info-container">
+        <div class="other-info">飞屎计划 @ 2022</div>
+        <div class="other-info">备案号</div>
+        <a target="_blank" rel="nofollow" href="https://beian.miit.gov.cn" class="other-info beian">
+          苏ICP备2022041752号-1</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref, getCurrentInstance } from "vue"
+import {getCurrentInstance, ref} from "vue"
 import dayRead from "../dayRead/dayRead.vue";
+let backgroundImage = ref('@/assets/background.png');
+
 
 let currentType = ref('dayRead')
 const that = getCurrentInstance().proxy
 
 function changeTab(tabName) {
-    switch (tabName) {
-        case 'dayRead':
-            currentType.value = 'dayRead'
-            break
-        case 'other':
-            that.$message.info('正在建设中！')
-            break
-        default:
-            break
-    }
+  switch (tabName) {
+    case 'dayRead':
+      currentType.value = 'dayRead'
+      backgroundImage.value = '@/assets/background-dayRead.png';
+      break
+    case 'other':
+      that.$message.info('正在建设中！')
+      break
+    case 'other':
+      that.$message.info('dayRead');
+      backgroundImage.value = '@/assets/background-other.png';
+      break;
+    default:
+      break
+  }
 }
 
 </script>
 
 <style scoped lang="scss">
 .home-page-container {
-    width: 100%;
+  width: 100%;
+  height: 100%;
+  line-height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #555555;
+
+  .main-background {
     height: 100%;
-    line-height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background: #555555;
+    width: 100%;
+    position: fixed;
+    z-index: 0;
+    background-image: url("@/assets/background.png");
+    background-image: url("@/assets/background1.png");
+    background-repeat: no-repeat;
+    background-size: 100%;
+    opacity: 0.5;
 
-    .main-background {
-        height: 100%;
-        width: 100%;
-        position: fixed;
-        z-index: 0;
-        background-image: url("@/assets/background.png");
-        background-repeat: no-repeat;
-        background-size: 100%;
-        opacity: 0.5;
+  }
+
+  .main-head {
+    width: 100%;
+    height: 10%;
+    background-image: url("@/assets/background.png");
+    background-repeat: no-repeat;
+    background-size: 100%;
+    user-select: none;
+    z-index: 999;
+    position: fixed;
+
+    .main-title {
+      float: left;
+      margin-left: 3%;
+
+      .main-title-name {
+        height: 2em;
+        line-height: 2em;
+        font-size: 2em;
+        font-weight: 600;
+      }
+
+      .main-title-description {
+        height: 1.5em;
+        line-height: 1.5em;
+        font-size: 1.5em;
+      }
     }
 
-    .main-head {
-        width: 100%;
-        height: 10%;
-        background-image: url("@/assets/background.png");
-        background-repeat: no-repeat;
-        background-size: 100%;
-        user-select: none;
-        z-index: 999;
-        position: fixed;
+    .main-tabs {
+      float: right;
+      display: flex;
+      height: 100%;
+      width: 30%;
+      align-items: flex-end;
 
-        .main-title {
-            float: left;
-            margin-left: 3%;
+      .main-tabs-item {
+        display: flex;
+        height: 70%;
+        text-align: center;
+        cursor: url('@/assets/cursor/pointer.cur'), pointer;
+        width: auto;
+        padding: 0px 10px;
+        justify-content: center;
+        align-items: flex-end;
+        transition: all 0.4s;
 
-            .main-title-name {
-                height: 2em;
-                line-height: 2em;
-                font-size: 2em;
-                font-weight: 600;
-            }
-
-            .main-title-description {
-                height: 1.5em;
-                line-height: 1.5em;
-                font-size: 1.5em;
-            }
+        span {
+          position: relative;
+          font-size: 1.8em;
+          height: 1.8em;
+          line-height: 1.8em;
+          margin-bottom: 10px;
+          font-weight: lighter;
         }
+      }
 
-        .main-tabs {
-            float: right;
-            display: flex;
-            height: 100%;
-            width: 30%;
-            align-items: flex-end;
+      .main-tabs-item:hover {
+        background-color: rgba(255, 255, 255, 0.8);
+        border-radius: 50px;
+        transition: all 0.4s;
+      }
 
-            .main-tabs-item {
-                display: flex;
-                height: 70%;
-                text-align: center;
-                cursor: url('@/assets/cursor/pointer.cur'), pointer;
-                width: auto;
-                padding: 0px 10px;
-                justify-content: center;
-                align-items: flex-end;
-                transition: all 0.4s;
+      .selected-main-tab {
+        background-color: rgba(255, 255, 255, 0.5);
+        border-radius: 5px;
+        transition: all 0.4s;
+      }
+    }
+  }
 
-                span {
-                    position: relative;
-                    font-size: 1.8em;
-                    height: 1.8em;
-                    line-height: 1.8em;
-                    margin-bottom: 10px;
-                    font-weight: lighter;
-                }
-            }
+  .main-container {
+    position: relative;
+    top: 10%;
+    height: 90%;
+    width: 100%;
+    overflow-y: auto;
+    z-index: 99;
 
-            .main-tabs-item:hover {
-                background-color: rgba(255, 255, 255, 0.8);
-                border-radius: 50px;
-                transition: all 0.4s;
-            }
-
-            .selected-main-tab {
-                background-color: rgba(255, 255, 255, 0.5);
-                border-radius: 5px;
-                transition: all 0.4s;
-            }
-        }
+    .day-read-container {
+      position: relative;
+      left: 15%;
+      height: auto;
+      min-height: 97%;
+      width: 70%;
+      background-color: rgba(85, 85, 85, 0.6);
     }
 
-    .main-container {
-        position: relative;
-        top: 10%;
-        height: 90%;
-        width: 100%;
-        overflow-y: auto;
-        z-index: 99;
+    .other-info-container {
+      display: flex;
+      height: 3%;
+      justify-content: center;
+      align-items: center;
+      background: rgba(255, 255, 255, 0.3);
 
-        .day-read-container {
-            position: relative;
-            left: 15%;
-            height: auto;
-            min-height: 97%;
-            width: 70%;
-            background-color: rgba(85, 85, 85, 0.6);
-        }
+      .other-info {
+        margin: 0px 5px;
+      }
 
-        .other-info-container {
-            display: flex;
-            height: 3%;
-            justify-content: center;
-            align-items: center;
-            background: rgba(255, 255, 255, 0.3);
-
-            .other-info {
-                margin: 0px 5px;
-            }
-
-            .beian {
-                color: #000000
-            }
-        }
+      .beian {
+        color: #000000
+      }
     }
+  }
 
-    ::-webkit-scrollbar {
-        width: 8px;
-    }
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
 
-    ::-webkit-scrollbar-track {
-        background-color: #ffffff80;
-        border-radius: 10px;
-    }
+  ::-webkit-scrollbar-track {
+    background-color: #ffffff80;
+    border-radius: 10px;
+  }
 
-    ::-webkit-scrollbar-thumb {
-        background-color: rgba(0, 0, 0, 0.6);
-        border-radius: 10px;
-    }
+  ::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.6);
+    border-radius: 10px;
+  }
 }
 </style>
--- src/main/resources/notShitProject/vite.config.js
+++ src/main/resources/notShitProject/vite.config.js
@@ -1,29 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  baseL: './',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    host: true,
-    port: '10000',
-    open: false,
-    https: false,
+    plugins: [vue()],
+    baseL: './',
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        host: true,
+        port: '10000',
+        open: false,
+        https: false,
 
-    proxy: {
-      '/api': {
-        target: 'http://81.68.219.210:7070',	//实际请求地址
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
+        proxy: {
+            '/api': {
+                target: 'http://81.68.219.210:7070',	//实际请求地址
+                // target: 'http://localhost:7070',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
     }
-  }
 })
